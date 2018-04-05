b03b185d7f261cbf0c68d6fa8b17306e8c014e49
yosyp <y.shvab+git@gmail.com>
2018-04-05 13:10:04 -0400

    new plot, concentration profiles

---
+++ hw5/plot_concentration.m
@@ -0,0 +1,41 @@
+%
+% This corresponds to:
+% HW5 Question 3
+% 1 step    % 2 khist   % 3 x
+% 4 y       % 5 z       % 6 vx
+% 7 vy      % 8 vz      % 9 Ep
+% 10 Ek     % 11 T      % 12 Etot
+% 13 type
+%
+clear;clc;
+
+% // MSE6270_MD uses the following units:
+% // x - [A], t - [ps], m - [Da], E - [eV], F - [eV/A], P - [Pa]
+% enunit = 1e4 * m0 / e;  % !< A constant used to convert [Da*A^2/ps] to [eV] 
+kb = 8.617385e-5;       % !< Boltzman constant [eV/K] 
+kb_jk = 1.38064852e-23; % !< Boltzman constant [J/K] 
+m0 = 1.6605402e-27;     % !< Atomic mass unit [kg]
+aps2ms = 100; % speed conversion: 1 A/ps = 100 m/s
+
+m_argon = m0*39.948;    % units: kg
+N = 1372;               % number of atoms 
+box_size = 40.8045;     % size of periodic boundary condition box (in x,y,z)
+
+directories = [
+     "nptT138", "T = 138 K"; ...
+];
+
+i = 1;
+datadir = "data";
+path = sprintf('%s/%s/', directories(i,1), datadir);
+files(1,:) = dir(sprintf('%s*.d', path));
+for k = 1:length(files(i,:))
+    snapshot{i,k} = dlmread( ...
+                    [path,files(i,k).name],' ');
+    for j = 1:N
+        q(k,j) = snapshot(i,k}(j,2);        
+        x(k,j) = snapshot{i,k}(j,3);
+        y(k,j) = snapshot{i,k}(j,4);
+        z(k,j) = snapshot{i,k}(j,5);
+    end
+end
