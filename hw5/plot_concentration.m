70d47b936b038333179a71ecbc8d531d17acdfd8
yosyp <y.shvab+git@gmail.com>
2018-04-06 12:14:12 -0400

    fixed finite difference simulation, report

--- hw5/plot_concentration.m
+++ hw5/plot_concentration.m
@@ -11,7 +11,7 @@ clear;clc;
 
 % // MSE6270_MD uses the following units:
 % // x - [A], t - [ps], m - [Da], E - [eV], F - [eV/A], P - [Pa]
-% enunit = 1e4 * m0 / e;  % !< A constant used to convert [Da*A^2/ps] to [eV] 
+% enunit = 1e4 * m0 / ex;  % !< A constant used to convert [Da*A^2/ps] to [eV] 
 kb = 8.617385e-5;       % !< Boltzman constant [eV/K] 
 kb_jk = 1.38064852e-23; % !< Boltzman constant [J/K] 
 m0 = 1.6605402e-27;     % !< Atomic mass unit [kg]
@@ -70,6 +70,20 @@ for i=1:length(db)-1
     end
 end
 
-figure;plot(db(1:end-1),conc(1:end-1,:)); xlim([min max]);
+figure;
+plot(db(1:end-1),conc(1:end-1,:),'LineWidth', 4);
+title('Concentration Profile at t=961 ps');
+legend('type A','RIGID','type B','location','North');
+xlim([min max]);
+xlabel('Z coordinate, [A]','FontWeight','bold','Color','black');
+ylabel('# of atoms in plane','FontWeight','bold','Color','black');
+grid on;
+xt = get(gca, 'XTick'); set(gca, 'FontSize', 16);  set(gca, 'LineWidth', 2);
+
+
+
+
+
+
 % figure;plot(db,conc(:,1)+conc(:,3)); xlim([min max]);
 
