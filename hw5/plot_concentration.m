91a32749cd1be62d333e1e376a21160d0009fb3f
yosyp <y.shvab+git@gmail.com>
2018-04-06 10:38:53 -0400

    concentration profile plot

--- hw5/plot_concentration.m
+++ hw5/plot_concentration.m
@@ -18,14 +18,15 @@ m0 = 1.6605402e-27;     % !< Atomic mass unit [kg]
 aps2ms = 100; % speed conversion: 1 A/ps = 100 m/s
 
 m_argon = m0*39.948;    % units: kg
-N = 1372;               % number of atoms 
-box_size = 40.8045;     % size of periodic boundary condition box (in x,y,z)
+N = 1280;               % number of atoms 
 
 directories = [
-     "nptT138", "T = 138 K"; ...
+     "part2", "T = 138 K"; ...
+%      "equilpart2", "T = 138 K"; ...
 ];
 
 i = 1;
+% datadir = "data_gather2708";
 datadir = "data";
 path = sprintf('%s/%s/', directories(i,1), datadir);
 files(1,:) = dir(sprintf('%s*.d', path));
@@ -33,9 +34,42 @@ for k = 1:length(files(i,:))
     snapshot{i,k} = dlmread( ...
                     [path,files(i,k).name],' ');
     for j = 1:N
-        q(k,j) = snapshot(i,k}(j,2);        
+        q(k,j) = snapshot{i,k}(j,2);
         x(k,j) = snapshot{i,k}(j,3);
         y(k,j) = snapshot{i,k}(j,4);
         z(k,j) = snapshot{i,k}(j,5);
     end
 end
+
+k=length(files(i,:));
+% k=1;
+% max = max( z(k,:) );
+% % min = min( z(k,:) );
+min = 0;
+max = 115.6;
+% bins = 11;
+bins = 44;
+dbin = (max-min)/(bins-1);
+db = min:dbin:max;
+
+% 11.56 
+conc = zeros(length(db),3);
+% conc = zeros(N,3);
+for i=1:length(db)-1
+    fprintf('more than %4.4f less than %4.4f\n', db(i), db(i)+dbin);
+    for j=1:N
+        if (z(k,j) >= db(i)) && (z(k,j) <= (db(i)+dbin))
+            if q(k,j) == 1
+                conc(i,1) = conc(i,1)+1;
+            elseif q(k,j) == 3
+                conc(i,2) = conc(i,2)+1;
+            elseif q(k,j) == 0
+                conc(i,3) = conc(i,3)+1;
+            end
+        end
+    end
+end
+
+figure;plot(db(1:end-1),conc(1:end-1,:)); xlim([min max]);
+% figure;plot(db,conc(:,1)+conc(:,3)); xlim([min max]);
+
